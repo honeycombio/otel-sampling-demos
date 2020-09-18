d537cda7a9ea48c192326d2a1d4d27f603a12060
Paul Osman <paul@honeycomb.io>
2020-09-18 15:06:00 -0500

    Update interface

--- golang/deterministic/main.go
+++ golang/deterministic/main.go
@@ -1,4 +1,4 @@
-package main
+ package main
 
 import (
 	"fmt"
@@ -13,9 +13,8 @@ import (
 	"go.opentelemetry.io/contrib/instrumentation/net/http/httptrace"
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
 
-	sampler "github.com/honeycombio/opentelemetry-sampler-go/honeycomb"
-
-	"github.com/honeycombio/opentelemetry-exporter-go/honeycomb"
+	honeycombsamplers "github.com/honeycombio/opentelemetry-samplers-go/honeycombsamplers"
+	honeycomb "github.com/honeycombio/opentelemetry-exporter-go/honeycomb"
 
 )
 
@@ -30,7 +29,7 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	sample, err := sampler.NewDeterministicSampler(5)
+	sample, err := honeycombsamplers.DeterministicSampler(5)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -41,9 +40,11 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	global.SetTraceProvider(tp)
 	tracer := global.Tracer("sampling-service")
 	mux := http.NewServeMux()
+
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		ctx := r.Context()
 		attrs, _, spanCtx := httptrace.Extract(ctx, r)
@@ -53,12 +54,14 @@ func main() {
 			trace.WithAttributes(attrs...),
 		)
 		upper := rand.Intn(10)
+
 		for i := 0; i < upper; i++ {
 			_, iSpan := tracer.Start(ctx, fmt.Sprintf("Sample-%d", i))
 			log.Printf("Doing really hard work (%d / %d)\n", i + 1, upper)
 			<-time.After(time.Duration(rand.Intn(1000)) * time.Millisecond)
 			iSpan.End()
 		}
+
 		defer span.End()
 		fmt.Fprintf(w, "Hello, World")
 	})
